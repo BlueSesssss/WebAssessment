b0f1c2e27490f35b2af805f4084c6a2837aba529
BlueSesssss <bluesieeeee@gmail.com>
2024-10-01 14:21:44 +0800

    Add conditionals and loops in JavaScript

--- java.js
+++ java.js
@@ -2,3 +2,23 @@ function showAlert() {
     alert("Alert")
 }
 
+function number() {
+    const userInput = prompt("Please enter a number:");
+    const number = parseInt(userInput, 10);
+
+    // Check if the number is even or odd
+    if (!isNaN(number)) {
+        if (number % 2 === 0) {
+            alert(number + " is even.");
+        } else {
+            alert(number + " is odd.");
+        }
+
+        // Log numbers from 1 to the user's number
+        for (let i = 1; i <= number; i++) {
+            console.log(i);
+        }
+    } else {
+        alert("That's not a valid number!");
+    }
+}
